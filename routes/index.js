a41d5f35bff368aa5dce800a1ade6b6d1c0dc8c3
Vincent Siauw <jamesvincentsiauw@gmail.com>
2019-11-27 19:56:49 +0700

    Fixed Route

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,6 @@ var router = express.Router();
 var requestify = require('requestify');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
 router.get('/',function(req, res, next){
     if (req.session.loggedIn){
         var data = true
